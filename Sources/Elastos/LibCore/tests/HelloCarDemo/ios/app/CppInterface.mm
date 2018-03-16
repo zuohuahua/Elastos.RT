39da096b45acf42bc792eeb031fbcecdbee3d3df
Joel Liang <liangyuzhou@gmail.com>
2018-03-16 10:50:58 +0800

    Remove the namespace of HelloCarDemo on IOS (#98)
    Also changed the file structure according to #95 to use the same code as other building environments.

--- Sources/Elastos/LibCore/tests/HelloCarDemo/ios/app/CppInterface.mm
+++ Sources/Elastos/LibCore/tests/HelloCarDemo/ios/app/CppInterface.mm
@@ -6,7 +6,6 @@
 @end
 
 #include <_Elastos.HelloCarDemo.h>
-using namespace Elastos::HelloCarDemo;
 
 @implementation CppInterface
 
@@ -20,7 +19,7 @@ using namespace Elastos::HelloCarDemo;
         ECode ec = myHelloCar->Hello(&name);
         if(FAILED(ec)) {
             printf("CHelloCar::New() error! ec = %x\n\n", ec);
-            return;
+            return nullptr;
         }
         printf("Hello %s\n", name.string());
     }
