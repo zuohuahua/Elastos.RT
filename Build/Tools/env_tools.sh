zuohuahua/Elastos.RT

commit 3f07eabf771b5b74555362c4790b51f17a1fbef5

Setup Travis CI and move eco files to XDK_TARGETS (#133)
Added .travis.yml for continuous integration
Modify env_tools.sh to return non-zero when build failed.
Change Dockerfile to adapt Travis CI
Added build status to README.md

Move eco to $XDK_TARGETS directory
Generate some of the static libraries to $XDK_USER_LIB directory

Build/Tools/env_tools.sh
@@ -677,7 +677,7 @@ function emake ()
                     echo "Could not find Android NDK (r15c or newer)."
                     echo "    You should set an environment variable:"
                     echo "      export ANDROID_NDK=~/my-android-ndk"
-                    return
+                    return 1
                 elif [ -f $ANDROID_NDK/build/cmake/android.toolchain.cmake ]; then
                     CMAKE_ARGS="$CMAKE_ARGS -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake"
                 else
@@ -685,7 +685,7 @@ function emake ()
                     echo "We only support Android NDK r15c or newer version."
                     echo "    Please set the environment variable properly:"
                     echo "      export ANDROID_NDK=~/my-android-ndk"
-                    return
+                    return 1
                 fi
             fi
 
@@ -746,9 +746,11 @@ function emake ()
 
                 if [ "$DISABLE_EMAKE_TIMES" == "1" ]; then
                     (cd $MIRROR_DIR && $XDK_MAKE $1 $2 $3 $4 $5 $6)
+                    BUILD_EXIT_CODE=$?
                 else
                     local START_TIME=`date +%s`
                     (cd $MIRROR_DIR && $XDK_MAKE $1 $2 $3 $4 $5 $6)
+                    BUILD_EXIT_CODE=$?
                     local ELAPSED_TIME=$(( `date +%s`-$START_TIME ))
                     local HOURS=`echo $ELAPSED_TIME/3600 | bc`
                     local MINUTES=`echo $ELAPSED_TIME/60%60 | bc`
@@ -756,6 +758,7 @@ function emake ()
                     echo "Build finished, elapsed time: $HOURS Hours, $MINUTES Minutes, $SECONDS Seconds."
                 fi
                 unset XDK_MAKE XDK_MAKEFILE XDK_EMAKE_DIR BUILD_VERBOSE TEST_COVERAGE
+                return $BUILD_EXIT_CODE
             fi
             if [ "$1" == "clean" ]; then
                 echo "Cleaning mirror direcotory..."

Sources/Elastos/LibCore/src/Lib/CMakeLists.txt
@@ -6,3 +6,7 @@ set(SOURCES
 )
 
 add_library(Elastos.CoreLibrary.UUIDs STATIC ${SOURCES})
+set_target_properties(Elastos.CoreLibrary.UUIDs
+    PROPERTIES
+    ARCHIVE_OUTPUT_DIRECTORY $ENV{XDK_USER_LIB}
+)

Sources/Elastos/LibCore/src/elastos/utility/CMakeLists.txt
@@ -90,7 +90,7 @@ set(SOURCES
 )
 
 add_library(elastos_utility_min STATIC ${SOURCES})
-xdk_combine_static_libraries(elastos.utility
+xdk_combine_static_libraries(${CMAKE_CURRENT_BINARY_DIR} elastos.utility
     $<TARGET_FILE:elastos_utility_min>
     $<TARGET_FILE:elastos_utility_regex>
 )

Sources/Elastos/Runtime/Core/eladll/lib/CMakeLists.txt
@@ -1,5 +1,5 @@
 
-xdk_combine_static_libraries(Elastos.Runtime_static
+xdk_combine_static_libraries($ENV{XDK_USER_LIB} Elastos.Runtime_static
     $<TARGET_FILE:lib4car>
     $<TARGET_FILE:elstring>
     $<TARGET_FILE:elrefbase>

Sources/Elastos/Runtime/Library/eltypes/CMakeLists.txt
@@ -19,7 +19,7 @@ endif()
 
 add_library(eltypes_min STATIC ${SOURCES})
 
-xdk_combine_static_libraries(eltypes
+xdk_combine_static_libraries(${CMAKE_CURRENT_BINARY_DIR} eltypes
     $<TARGET_FILE:eltypes_min>
     $<TARGET_FILE:elrefbase>
     $<TARGET_FILE:elstring>

Sources/Sample/HelloCarDemo/iOS/CMakeLists.txt
@@ -6,6 +6,10 @@ set(SOURCES
 )
 
 add_library(Elastos.HelloCarDemo.UUIDs STATIC ${SOURCES})
+set_target_properties(Elastos.HelloCarDemo.UUIDs
+    PROPERTIES
+    ARCHIVE_OUTPUT_DIRECTORY $ENV{XDK_USER_LIB}
+)
 
 set (APP_NAME HelloCarApp)
 set (APP_SUBDIR app)

Sources/Sample/HelloCarDemo/iOS/app/CMakeLists.txt
@@ -84,12 +84,12 @@ set(CMAKE_SHARED_LIBRARY_RUNTIME_C_FLAG "-Wl,-rpath,")
 set(CMAKE_SHARED_LIBRARY_RUNTIME_C_FLAG_SEP ":")
 
 add_library( Elastos.Runtime SHARED IMPORTED GLOBAL )
-set_target_properties( Elastos.Runtime PROPERTIES IMPORTED_LOCATION $ENV{XDK_USER_LIB}/Elastos.Runtime.eco )
+set_target_properties( Elastos.Runtime PROPERTIES IMPORTED_LOCATION $ENV{XDK_TARGETS}/Elastos.Runtime.eco )
 target_link_libraries(${APP_NAME} Elastos.Runtime)
 
 target_link_libraries(${APP_NAME}
-                    ${CMAKE_BINARY_DIR}/../../../../../Elastos/Runtime/Core/eladll/lib/libElastos.Runtime_static.a
-                    ${CMAKE_BINARY_DIR}/../../libElastos.HelloCarDemo.UUIDs.a)
+    $ENV{XDK_USER_LIB}/libElastos.Runtime_static.a
+    $ENV{XDK_USER_LIB}/libElastos.HelloCarDemo.UUIDs.a)
 
 
 # Turn on ARC
@@ -157,9 +157,9 @@ add_custom_command(
     POST_BUILD COMMAND /bin/sh -c
     \"COMMAND_DONE=0 \;
     if ${CMAKE_COMMAND} -E copy
-        $ENV{XDK_USER_LIB}/Elastos.Runtime.eco
-        $ENV{XDK_USER_LIB}/Elastos.CoreLibrary.eco
-        $ENV{XDK_USER_LIB}/Elastos.HelloCarDemo.eco
+        $ENV{XDK_TARGETS}/Elastos.Runtime.eco
+        $ENV{XDK_TARGETS}/Elastos.CoreLibrary.eco
+        $ENV{XDK_TARGETS}/Elastos.HelloCarDemo.eco
         \${BUILT_PRODUCTS_DIR}/${APP_NAME}.app/Frameworks/
         \&\>/dev/null \; then
         COMMAND_DONE=1 \;
@@ -193,6 +193,10 @@ add_custom_command(
         --sign ${CODE_SIGN_IDENTITY}
         \&\>/dev/null \; then
         COMMAND_DONE=1 \;
+    fi \;
+    if [ -n '$ENV{XDK_SKIP_CODE_SIGN}' ] \; then
+        echo Framework codesign skiped \;
+        exit 0 \;
     fi \;
     if [ \\$$COMMAND_DONE -eq 0 ] \; then
         echo Framework codesign failed \;

Sources/ToolChains/Carl/ElCar/Carc/clsbase/CMakeLists.txt
@@ -20,7 +20,7 @@ set(SOURCES
 include_directories(${CMAKE_CURRENT_SOURCE_DIR}/../../inc)
 add_library(clsbase_min STATIC ${SOURCES})
 
-xdk_combine_static_libraries(clsbase
+xdk_combine_static_libraries(${CMAKE_CURRENT_BINARY_DIR} clsbase
     $<TARGET_FILE:clsbase_min>
     $<TARGET_FILE:guidseed>
     $<TARGET_FILE:clsinfo>

commit a5336d4aa4eeab96f49e3d22be41957cb3f96ebb

Added ElaCarrier submodule and added cmake file for out-of-source build (#135)
Also fixed 'emake clean' issue which introduced in #133

.gitmodules
@@ -4,3 +4,6 @@
 [submodule "Sources/ThirdParty/icu4c"]
 	path = Sources/ThirdParty/icu4c
 	url = https://github.com/sillsdev/icu4c.git
+[submodule "Sources/ThirdParty/Elastos.NET.Carrier.Native.SDK"]
+	path = Sources/ThirdParty/ElaCarrier
+	url = https://github.com/elastos/Elastos.NET.Carrier.Native.SDK.git

Build/Tools/env_tools.sh
@@ -691,6 +691,7 @@ function emake ()
 
             local MIRROR_ROOT_DIR=$XDK_USER_OBJ/$XDK_BUILD_KIND/mirror
             local MIRROR_DIR=$MIRROR_ROOT_DIR$XDK_EMAKE_DIR
+            BUILD_EXIT_CODE=0
             if [ ! "${1%.car}" == "$1" ]; then
                 carc -i -c ${1%.car}.cls $1
                 if [ "$?" == "0" ]; then
@@ -758,12 +759,12 @@ function emake ()
                     echo "Build finished, elapsed time: $HOURS Hours, $MINUTES Minutes, $SECONDS Seconds."
                 fi
                 unset XDK_MAKE XDK_MAKEFILE XDK_EMAKE_DIR BUILD_VERBOSE TEST_COVERAGE
-                return $BUILD_EXIT_CODE
             fi
             if [ "$1" == "clean" ]; then
                 echo "Cleaning mirror direcotory..."
                 rm -rf $MIRROR_DIR 1>/dev/null
             fi
+            return $BUILD_EXIT_CODE
         fi
     fi
 }

Sources/ThirdParty/CMakeLists.txt
@@ -2,4 +2,6 @@
 if("$ENV{XDK_TARGET_PLATFORM}" STREQUAL "ios")
     include(openssl.cmake)
     include(icu.cmake)
+elseif("$ENV{XDK_TARGET_PLATFORM}" STREQUAL "android")
+    include(elacarrier.cmake)
 endif()

Sources/ThirdParty/elacarrier.cmake
@@ -1,0 +1,64 @@
+
+
+set (ELACARRIER_SCRIPTS_DIR ${CMAKE_CURRENT_SOURCE_DIR}/scripts)
+set (ELACARRIER_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/ElaCarrier)
+set (ELACARRIER_BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/ElaCarrier)
+
+file(MAKE_DIRECTORY ${ELACARRIER_BINARY_DIR})
+
+add_custom_command(
+    COMMENT "Preparing elacarrier for out-of-source build..."
+    OUTPUT ${ELACARRIER_BINARY_DIR}/build/Makefile
+    COMMAND ${ELACARRIER_SCRIPTS_DIR}/prepare_out_of_source_build.sh ${ELACARRIER_SOURCE_DIR}
+    WORKING_DIRECTORY ${ELACARRIER_BINARY_DIR}
+    DEPENDS ${ELACARRIER_SOURCE_DIR}/build/Makefile
+)
+
+add_custom_command(
+    COMMENT "Downloading flatcc..."
+    OUTPUT "${ELACARRIER_BINARY_DIR}/build/_build/.tarballs/flatcc-0.5.0.tar.gz"
+    COMMAND mkdir -p ${ELACARRIER_BINARY_DIR}/build/_build/.tarballs
+    COMMAND curl -L -o _build/.tarballs/flatcc-0.5.0.tar.gz https://github.com/dvidelabs/flatcc/archive/v0.5.0.tar.gz
+    WORKING_DIRECTORY ${ELACARRIER_BINARY_DIR}/build
+    DEPENDS ${ELACARRIER_BINARY_DIR}/build/Makefile
+)
+
+add_custom_command(
+    COMMENT "Building flatcc..."
+    OUTPUT "${ELACARRIER_BINARY_DIR}/build/flatcc-0.5.0/bin/flatcc"
+    COMMAND tar xzvf _build/.tarballs/flatcc-0.5.0.tar.gz
+    COMMAND mkdir -p flatcc-0.5.0/build/install && cd flatcc-0.5.0/build/install && cmake ../.. && make
+    WORKING_DIRECTORY ${ELACARRIER_BINARY_DIR}/build
+    DEPENDS "${ELACARRIER_BINARY_DIR}/build/_build/.tarballs/flatcc-0.5.0.tar.gz"
+)
+
+add_custom_command(
+    COMMENT "Building elacarrier..."
+    OUTPUT "${ELACARRIER_BINARY_DIR}/build/_dist/Android-arm/debug/lib/libelacommon.so"
+           "${ELACARRIER_BINARY_DIR}/build/_dist/Android-arm/debug/lib/libelacarrier.so"
+           "${ELACARRIER_BINARY_DIR}/build/_dist/Android-arm/debug/lib/libelasession.so"
+    COMMAND ANDROID_NDK_HOME=$ENV{ANDROID_NDK} PATH=${ELACARRIER_BINARY_DIR}/build/flatcc-0.5.0/bin:$ENV{PATH} ./android_build.sh arm
+    WORKING_DIRECTORY ${ELACARRIER_BINARY_DIR}/build
+    DEPENDS "${ELACARRIER_BINARY_DIR}/build/flatcc-0.5.0/bin/flatcc"
+)
+
+add_custom_target(build_elacarrier ALL DEPENDS
+    "${ELACARRIER_BINARY_DIR}/build/_dist/Android-arm/debug/lib/libelacommon.so"
+    "${ELACARRIER_BINARY_DIR}/build/_dist/Android-arm/debug/lib/libelacarrier.so"
+    "${ELACARRIER_BINARY_DIR}/build/_dist/Android-arm/debug/lib/libelasession.so")
+
+add_library( elacommon STATIC IMPORTED GLOBAL )
+add_library( elacarrier STATIC IMPORTED GLOBAL )
+add_library( elasession STATIC IMPORTED GLOBAL )
+
+set_target_properties( elacommon PROPERTIES IMPORTED_LOCATION
+    "${ELACARRIER_BINARY_DIR}/build/_dist/Android-arm/debug/lib/libelacommon.so")
+set_target_properties( elacarrier PROPERTIES IMPORTED_LOCATION
+    "${ELACARRIER_BINARY_DIR}/build/_dist/Android-arm/debug/lib/libelacarrier.so")
+set_target_properties( elasession PROPERTIES IMPORTED_LOCATION
+    "${ELACARRIER_BINARY_DIR}/build/_dist/Android-arm/debug/lib/libelasession.so")
+
+add_dependencies( elacommon build_elacarrier )
+add_dependencies( elacarrier build_elacarrier )
+add_dependencies( elasession build_elacarrier )
+

Sources/ThirdParty/openssl.cmake
@@ -18,7 +18,7 @@ get_filename_component(OPENSSL_CROSS_SDK ${CMAKE_IOS_SDK_ROOT} NAME)
 add_custom_command(
     COMMENT "Preparing openssl for out-of-source build..."
     OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/openssl/Configure
-    COMMAND ${OPENSSL_SCRIPTS}/prepare_openssl.sh ${OPENSSL_SOURCE}
+    COMMAND ${OPENSSL_SCRIPTS}/prepare_out_of_source_build.sh ${OPENSSL_SOURCE}
     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/openssl
     DEPENDS ${OPENSSL_SOURCE}/Configure
 )

Sources/ThirdParty/scripts/prepare_openssl.sh
@@ -1,19 +1,0 @@
-#!/bin/bash
-
-if [ $# -ne 1 ]; then
-    echo "usage: $(basename $0) openssl_source"
-    exit 1
-fi
-
-OPENSSL_SOURCE=$1
-
-(cd $OPENSSL_SOURCE; find . -type f) | while read F; do
-    DIR_NAME=`dirname $F`
-    if [ ! -d $DIR_NAME ]; then
-        mkdir -p $DIR_NAME
-        echo $DIR_NAME '->' $OPENSSL_SOURCE/$DIR_NAME
-    fi
-    rm -f $F; ln -s $OPENSSL_SOURCE/$F $F
-done
-echo
-make -f Makefile.org clean

Sources/ThirdParty/scripts/prepare_out_of_source_build.sh
@@ -1,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "usage: $(basename $0) source_dir"
+    exit 1
+fi
+
+SOURCE_DIR=$1
+
+(cd $SOURCE_DIR; find . -type f) | while read F; do
+    DIR_NAME=`dirname $F`
+    if [ ! -d $DIR_NAME ]; then
+        mkdir -p $DIR_NAME
+        echo $DIR_NAME '->' $SOURCE_DIR/$DIR_NAME
+    fi
+    rm -f $F; ln -s $SOURCE_DIR/$F $F
+done
+echo