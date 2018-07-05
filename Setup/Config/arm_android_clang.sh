4f2804bd310f00ccf94fa8885beceb41360b565a
zuohuahua <34299909+zuohuahua@users.noreply.github.com>
2018-07-05 10:58:18 +0800

    copy libc++_shared.so to sdk library in android (#245)
    * copy libc++_shared.so to sdk library in android
    
    define evn ANDROID_ABI
    
    * set CMAKE_ARGS by env

--- Setup/Config/arm_android_clang.sh
+++ Setup/Config/arm_android_clang.sh
@@ -4,12 +4,13 @@ export XDK_COMPILER=clang
 export XDK_TARGET_CPU=arm
 if [[ ! "$_ELASTOS64" == "" ]]; then
     export XDK_TARGET_CPU_ARCH=64
-    export CMAKE_ARGS="-DANDROID_ABI=arm64-v8a"
+    export ANDROID_ABI=arm64-v8a
 else
     export XDK_TARGET_CPU_ARCH=32
-    export CMAKE_ARGS="-DANDROID_ABI=armeabi-v7a"
+    export ANDROID_ABI=armeabi-v7a
 fi
 
+export CMAKE_ARGS="-DANDROID_ABI=$ANDROID_ABI"
 export CMAKE_ARGS="$CMAKE_ARGS -DANDROID_TOOLCHAIN=$XDK_COMPILER"
 export CMAKE_ARGS="$CMAKE_ARGS -DANDROID_PLATFORM=android-21"
 export CMAKE_ARGS="$CMAKE_ARGS -DANDROID_STL=c++_shared"
